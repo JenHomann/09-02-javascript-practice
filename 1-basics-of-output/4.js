8be8f8a95ca422f2c33ccfe1513266523fdb9153
“Jen <mpjeno@gmail.com>
2014-09-02 10:21:52 -0500

    Completed section 1 - basics of output

---
+++ 1-basics-of-output/4.js
@@ -0,0 +1,4 @@
+//What happens if you use string concatenation, i.e. console.log("hel" + "lo")?
+
+
+console.log("hel" + "lo" + "\nworld")
---
+++ 1-basics-of-output/6.js
@@ -0,0 +1,5 @@
+//What happens if you use concatenation with a string and an integer, i.e. console.log("The meaning of life is " + 42). Is this behavior the same or different as in Ruby?
+
+console.log("The meaning of life is " + 42)
+
+//this behavior is different than in Ruby, as JS will just ad 42 to the string whereas Ruby will try to literally add 42 to "THe meaning of life" which will throw an error
